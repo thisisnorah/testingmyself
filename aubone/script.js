356b5d27e016ab3cbfcfde4654b5907cad332fb5
thisisnorah <hamgoverygood@gmail.com>
2024-10-27 22:09:33 +0900

    Delete aubone directory

--- aubone/script.js
+++
@@ -1,9 +0,0 @@
-function showBoneDetail(boneName, boneDescription) {
-  document.getElementById('bone-name').innerText = boneName;
-  document.getElementById('bone-description').innerText = boneDescription;
-  document.getElementById('bone-detail').classList.remove('hidden');
-}
-
-function hideBoneDetail() {
-  document.getElementById('bone-detail').classList.add('hidden');
-}
